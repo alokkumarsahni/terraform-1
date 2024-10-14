f915ba24bc39c4c465b726edda88338bf27ac11d
alokkumarsahni <113391506+alokkumarsahni@users.noreply.github.com>
2024-10-14 23:21:49 +0530

    Update tera-1.tf

--- tera-1.tf
+++ tera-1.tf
@@ -28,24 +28,64 @@ module "vpc" {
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+										
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  #enable_vpn_gateway = true
 
   tags = {
     Terraform = "true"
     Environment = "dev"
+
+	}					 
+										 
+   
+ 
+
+	module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }		 
   }
 
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
-}
+
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     ami_type       = "ami-0b0ea68c435eb488d"
     instance_types = ["t2.medium"]
 
     attach_cluster_primary_security_group = true
+}
+
+
+eks_managed_node_groups = {
+    ascode-cluster-wg = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+
+    }
+  }
+
+  tags = local.tags
 }
